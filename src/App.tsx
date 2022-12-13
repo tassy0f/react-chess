09e05fb8dcf6b8d056fcc2c0bf4e54eaae2273a3
tassy0f <7243180roma65@gmail.com>
2022-12-13 13:44:59 +0300

    #1 Начало
    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import BoardComponent  from "./components/BoardComponent";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <BoardComponent/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/BoardComponent.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+const BoardComponent = () => {
+    return (
+        <div className='board'>
+            <div className='ceil white'></div>
+            <div className='ceil black'></div>
+            <div className='ceil white'></div>
+            <div className='ceil black'></div>
+            <div className='ceil white'></div>
+            <div className='ceil black'></div>
+            <div className='ceil white'></div>
+            <div className='ceil black'></div>
+            <div className='ceil white'></div>
+            <div className='ceil black'></div>
+            <div className='ceil white'></div>
+            <div className='ceil black'></div>
+            <div className='ceil white'></div>
+            <div className='ceil black'></div>
+            <div className='ceil white'></div>
+            <div className='ceil black'></div>
+            <div className='ceil white'></div>
+            <div className='ceil black'></div>
+            <div className='ceil white'></div>
+            <div className='ceil black'></div>
+            <div className='ceil white'></div>
+            <div className='ceil black'></div>
+            <div className='ceil white'></div>
+            <div className='ceil black'></div>
+            <div className='ceil white'></div>
+            <div className='ceil black'></div>
+            <div className='ceil white'></div>
+            <div className='ceil black'></div>        </div>
+    )
+}
+
+export default BoardComponent;
